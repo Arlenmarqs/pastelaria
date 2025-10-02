e7c467938093766c515e49a69b458a6eed24d087
Arlenmarqs <arlenmarqs@gmail.com>
2025-10-02 00:44:56 -0300

    la vai

---
+++ js/list.js
@@ -0,0 +1,172 @@
+const pasteis = [
+  {
+    id: 1,
+    nome: "Pastel de Palmito",
+    preco: 7.50,
+    ingredientes: "Palmito, Cebola, Azeitona, Temperos",
+    tamanho: "Médio"
+  },
+  {
+    id: 2,
+    nome: "Pastel de Pizza",
+    preco: 8.00,
+    ingredientes: "Presunto, Mussarela, Tomate, Orégano",
+    tamanho: "Grande"
+  },
+  {
+    id: 3,
+    nome: "Pastel de Carne Seca com Catupiry",
+    preco: 9.00,
+    ingredientes: "Carne seca, Catupiry, Cebola",
+    tamanho: "Grande"
+  },
+  {
+    id: 4,
+    nome: "Pastel de Bacalhau",
+    preco: 10.00,
+    ingredientes: "Bacalhau, Batata, Cebola, Azeitona",
+    tamanho: "Grande"
+  },
+  {
+    id: 5,
+    nome: "Pastel de Banana com Canela",
+    preco: 6.50,
+    ingredientes: "Banana, Canela, Açúcar",
+    tamanho: "Médio"
+  },
+  {
+    id: 6,
+    nome: "Pastel de Doce de Leite",
+    preco: 7.00,
+    ingredientes: "Doce de leite, Chocolate branco",
+    tamanho: "Pequeno"
+  },
+  {
+    id: 7,
+    nome: "Pastel de Queijo com Orégano",
+    preco: 6.50,
+    ingredientes: "Mussarela, Orégano",
+    tamanho: "Médio"
+  },
+  {
+    id: 8,
+    nome: "Pastel de Frango com Requeijão",
+    preco: 8.50,
+    ingredientes: "Frango desfiado, Requeijão, Cebola",
+    tamanho: "Médio"
+  },
+  {
+    id: 9,
+    nome: "Pastel de Camarão",
+    preco: 11.00,
+    ingredientes: "Camarão, Requeijão, Cebola, Salsinha",
+    tamanho: "Grande"
+  },
+  {
+    id: 10,
+    nome: "Pastel de Chocolate com Morango",
+    preco: 9.50,
+    ingredientes: "Chocolate ao leite, Morango, Granulado",
+    tamanho: "Médio"
+  },
+  {
+    id: 11,
+    nome: "Pastel de vento",
+    preco: 5.50,
+    ingredientes: "Vento e massa",
+    tamanho: "Grande"
+}
+];
+
+let body = document.querySelector("body")
+function renderProducts() {
+    let tbody = document.querySelector("tbody")
+
+    for (let index = 0; index < pasteis.length; index++) {
+        let tr = document.createElement("tr")
+        tr.innerHTML = `
+                <td>${pasteis[index].id}</td>
+                <td>${pasteis[index].nome}</td>
+                <td>${pasteis[index].preco}</td>
+                <td>${pasteis[index].ingredientes}</td>
+                <td>${pasteis[index].tamanho}</td>
+                <td>
+                    <button class="btn btn-warning">Editar</button>
+                    <button  onclick="deletarPasteis(${pasteis[index].id})" class="btn btn-danger">Excluir</button>
+                </td>
+        `
+        tbody.appendChild(tr)
+    }
+}
+renderProducts()
+
+function deletarPasteis(id) {
+  let tbody = document.querySelector("tbody")
+  let pastel = pasteis.find((pastel) => pastel.id === id)
+  pasteis.splice(pastel,1)
+  tbody.innerHTML = ""
+  renderProducts()
+}
+
+
+function renderModal() {
+    let div = document.createElement("div")
+    div.classList.add("modal-overlay")
+    div.innerHTML = `
+        <div id="createModal" class="modal-content">
+          <form action="">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Digite seu Id" aria-label="Id" aria-describedby="basic-addon1" id="inputId">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Digite seu nome" aria-label="Id" aria-describedby="basic-addon1" id="inputNome">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Digite seu preço" aria-label="Id" aria-describedby="basic-addon1" id="inputPreco">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Digite seu ingredientes" aria-label="Id" aria-describedby="basic-addon1" id="inputIngredientes">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Digite seu tamanho" aria-label="Id" aria-describedby="basic-addon1" id="inputTamanho">
+            </div>
+          </form>
+
+        <div id="div-buttons">
+          <button onclick="removeModal()" type="button" class="btn btn-secondary">Fechar</button>
+
+          <button type="button" onclick="criarPastel()" class="btn btn-primary">Enviar</button>
+        </div>
+        </div>
+    `
+    body.appendChild(div)
+}
+
+function removeModal() {
+  let createModal = document.querySelector(".modal-overlay")
+  body.removeChild(createModal)
+}
+
+function criarPastel()  {
+    let id = document.querySelector("#inputId").value
+    let nome = document.querySelector("#inputNome").value
+    let preco = document.querySelector("#inputPreco").value
+    let ingredientes = document.querySelector("#inputIngredientes").value
+    let tamanho = document.querySelector("#inputTamanho").value
+    let tbody = document.querySelector("tbody")
+
+    pasteis.push({
+      id:id,
+      nome:nome,
+      preco:preco,
+      ingredientes:ingredientes,
+      tamanho:tamanho
+    })
+    
+    removeModal()
+    tbody.innerHTML = ""
+    renderProducts()
+    
+}
+
+
